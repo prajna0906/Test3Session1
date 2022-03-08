e3a01a1b285ecd0d7fc524e330c8fcc5637eff60
PRAJNA R PATIL <93109368+prajna0906@users.noreply.github.com>
2022-03-08 04:24:48 +0000

    should work on it

--- p1original.c
+++ p1original.c
@@ -12,6 +12,7 @@ void input(int*num1, int *den1,, int *num2, int *den2)
     printf("enter the numerator and denominator of the second fraction\t");
     scanf("%d%d",num2,den2);
 }
+
 void add(int num1,int den1, int num2, int den2, int *num3, int *den3)
 {
     *num3=num1*den2+num2*den1;
---
+++ p2original.c
@@ -0,0 +1,27 @@
+/*struct _fraction
+{
+int num,den;
+};
+typedef _fraction Fraction
+Fraction input_fraction();
+Fraction Largest_fraction(Fraction f1, Fraction f2, Fraction f3)
+void output(Fraction f1, Fraction f2, Fraction f3, Fraction largest)*/
+
+#include<stdio.h>
+struct _fraction
+{
+int num,den;
+};
+
+fraction input_fraction()
+{
+    fraction f
+    printf("enter the num and den\t");
+    scanf("%d%d",&f.num,&f.den);
+    return f;
+}
+
+
+
+
+
