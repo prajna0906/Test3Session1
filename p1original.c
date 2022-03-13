9114e0658213547c7609df23bdda93f33f49efb7
PRAJNA R PATIL <93109368+prajna0906@users.noreply.github.com>
2022-03-13 14:05:53 +0530

    donee
    donee

--- p1original.c
+++ p1original.c
@@ -3,19 +3,19 @@ void input(int *num1, int *den1, int *num2, int *den2);
 void add(int num1,int den1, int num2, int den2, int *num3, int *den3);
 void output(int num1, int den1, int num2, int den2, int num3, int den3);
 input:*/
-
 #include<stdio.h>
-void input(int*num1, int *den1,, int *num2, int *den2)
+void input(int*num1, int *den1, int *num2, int *den2)
 {
     printf("enter the numerator and denominator of thr first fraction \t");
-    scanf("%d%d",num1,den1);
+    scanf("%d %d",num1,den1);
     printf("enter the numerator and denominator of the second fraction\t");
-    scanf("%d%d",num2,den2);
+    scanf("%d %d",num2,den2);
 }
 
 void add(int num1,int den1, int num2, int den2, int *num3, int *den3)
 {
     *num3=num1*den2+num2*den1;
+    *den3=den1*den2;
 }
 void output(int num1, int den1, int num2, int den2, int num3, int den3)
 {
@@ -23,10 +23,9 @@ void output(int num1, int den1, int num2, int den2, int num3, int den3)
 }
 int main()
 {
-    int num1,nnum2,num3,den1,den2,den3;
+    int num1,num2,num3,den1,den2,den3;
     input(&num1,&den1,&num2,&den2);
-    add(num1,den1.num2,den2)
+    add(num1,den1,num2,den2,&num3,&den3);
     output(num1,den1,num2,den2,num3,den3);
     return 0;
-    
 }
